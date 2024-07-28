df472f7091b7823492d8351b3f6f150111777540
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2024-07-29 01:18:07 +0800

    Finished Home

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelector('.nav-links');
 
     burger.addEventListener('change', () => {
-        navLinks.classList.toggle('nav-active');
+        if (burger.checked) {
+            navLinks.classList.remove('nav-inactive');
+            navLinks.classList.add('nav-active');
+            navLinks.style.display = 'flex';
+        } else {
+            navLinks.classList.remove('nav-active');
+            navLinks.classList.add('nav-inactive');
+        }
+    });
+
+    navLinks.addEventListener('animationend', (event) => {
+        if (event.animationName === 'slideUp') {
+            navLinks.style.display = 'none';
+        }
     });
 });
